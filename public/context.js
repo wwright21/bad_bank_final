cbfd703813125c9e01be129a4d970dd423fc652f
arc-research-analytics <wwright@atlantaregional.org>
2024-02-05 18:03:39 -0500

    got everything running

--- public/context.js
+++ public/context.js
@@ -1,7 +1,6 @@
 const Route = ReactRouterDOM.Route;
 const Link = ReactRouterDOM.Link;
 const HashRouter = ReactRouterDOM.HashRouter;
-// const UserContext = React.createContext(null);
 
 function Card(props) {
   function classes() {
--- public/index.js
+++ public/index.js
@@ -3,18 +3,6 @@ function Spa() {
     <HashRouter>
       <div>
         <NavBar />
-        {/* <UserContext.Provider
-          value={{
-            users: [
-              {
-                name: "abel",
-                email: "abel@mit.edu",
-                password: "secret",
-                balance: 100,
-              },
-            ],
-          }}
-        > */}
         <div className="container" style={{ padding: "20px" }}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
@@ -24,7 +12,6 @@ function Spa() {
           <Route path="/balance/" component={Balance} />
           <Route path="/alldata/" component={AllData} />
         </div>
-        {/* </UserContext.Provider> */}
       </div>
     </HashRouter>
   );
